af24096e96ef707552497343833b62666099f964
Franklin Moody <fmoody@moodman.org>
2021-12-12 19:37:26 -0500

    Day 7 Puzzle 2 completed

--- 7/Day7Puzzle.py
+++ 7/Day7Puzzle.py
@@ -1,4 +1,5 @@
 # Advent of Code 2021 Day 6
+from functools import cache 
 
 def main():
     #input_file_name = "test_input"
@@ -9,12 +10,17 @@ def main():
     minimal_fuel : int = None
 
     for index in range(min(crab_positions), max(crab_positions)+1):
-        fuel = sum([abs(pos - index) for pos in crab_positions])
+        fuel = sum([calc_fuel_cost(pos, index) for pos in crab_positions])
         if not minimal_fuel or fuel < minimal_fuel:
             minimal_position = index
             minimal_fuel = fuel
 
     print(f"Minimum fuel use: {minimal_fuel}")
+
+@cache
+def calc_fuel_cost(pos : int, index : int) -> int:
+    #return abs(pos - index)
+    return sum(range(abs(pos-index)+1))
 
 def read_input(input_file_name : str) -> list:
     with open(input_file_name) as input_file:
